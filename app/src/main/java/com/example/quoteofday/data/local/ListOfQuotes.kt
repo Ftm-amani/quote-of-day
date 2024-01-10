52c92f34ed2f500c48c4a9aee3aba6f9d03f2169
ftm-amani <ftm.amani95@gmail.com>
2024-01-11 00:10:50 +0330

    refactor ChooseCategoriesScreen UI

--- app/src/main/java/com/example/quoteofday/data/local/ListOfQuotes.kt
+++ app/src/main/java/com/example/quoteofday/data/local/ListOfQuotes.kt
@@ -1,7 +1,6 @@
 package com.example.quoteofday.data.local
 
 import com.example.quoteofday.data.models.Quotes
-import com.example.quoteofday.data.QuotesTypeName
 
 object ListOfQuotes {
     var allQuotes = listOf(
@@ -10,7 +9,7 @@ object ListOfQuotes {
         Quotes(2, "there is a hope", "Hope", isFave = false, isSelected = true),
         Quotes(3, "MENTAL_HEALTH", "Mental Health", isFave = false, isSelected = true),
         Quotes(4, "be strong", "Money", isFave = false, isSelected = true),
-        Quotes(5, "don't give up", "Healthy Relationships", isFave = false, isSelected = true),
+        Quotes(5, "don't give up", "Relationships", isFave = false, isSelected = true),
         Quotes(6, "you are cute", "Motivational", isFave = false, isSelected = true),
         Quotes(7, "love u", "Self Confidence", isFave = false, isSelected = true),
         Quotes(8, "قوی باش ادامه بده", "Mental Health", isFave = false, isSelected = true),
--- app/src/main/java/com/example/quoteofday/ui/setting/category/ChooseCategoriesScreen.kt
+++ app/src/main/java/com/example/quoteofday/ui/setting/category/ChooseCategoriesScreen.kt
@@ -1,19 +1,26 @@
 package com.example.quoteofday.ui.setting.category
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material3.Button
-import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.remember
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.quoteofday.data.models.*
@@ -29,7 +36,7 @@ fun ChooseCategoryScreen(
     viewModel: ChooseCategoriesViewModel,
 ) {
     val distinctQuotes by viewModel.distinctQuoteNamesAndSelection.collectAsState(emptyList())
-    val selectedCategories = mutableListOf<QuoteNameAndSelection>()
+    val selectedCategories = remember { mutableStateListOf<QuoteNameAndSelection>() }
 
     Column(
         modifier = Modifier
@@ -41,37 +48,65 @@ fun ChooseCategoryScreen(
     ) {
 
         Text(
-            text = "Choose what kind of Quotes you wanna see",
+            text = "Choose Categories",
+            fontWeight = FontWeight.Bold,
             style = MaterialTheme.typography.headlineMedium,
-            modifier = Modifier.padding(10.dp)
+            modifier = Modifier.padding(20.dp)
         )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+        ) {
+            Text(
+                text = "Select All categories that interest you.",
+                style = MaterialTheme.typography.bodyLarge,
+                textAlign = TextAlign.Start
+            )
+        }
 
-        LazyColumn {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            modifier = Modifier.fillMaxWidth()
+        ) {
             items(distinctQuotes) { category ->
-                val checked = remember { mutableStateOf(category.is_selected) }
+                val isSelected = remember { mutableStateOf(category.is_selected) }
 
-                Column {
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                        .border(
+                            width = 2.dp,
+                            color = if (isSelected.value) Color.DarkGray else Color.LightGray,
+                            shape = RoundedCornerShape(8.dp),
+                        )
+                        .clickable {
+                            isSelected.value = !isSelected.value
+                            val quoteType =
+                                QuoteNameAndSelection(category.quote_name, isSelected.value)
+
+                            if (isSelected.value) {
+                                if (!selectedCategories.contains(quoteType)) selectedCategories
+                                    .add(quoteType)
+                            } else {
+                                if (!selectedCategories.contains(quoteType)) selectedCategories
+                                    .add(quoteType)
+                            }
+                        }
+                ) {
                     Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
                         horizontalArrangement = Arrangement.Center,
                         verticalAlignment = Alignment.CenterVertically,
                     ) {
-                        Checkbox(
-                            checked = checked.value,
-                            onCheckedChange = { isChecked ->
-                                checked.value = isChecked
-                                val quoteType =
-                                    QuoteNameAndSelection(category.quote_name, checked.value)
-                                if (isChecked) {
-                                    if (!selectedCategories.contains(quoteType)) selectedCategories
-                                        .add(quoteType)
-                                } else {
-                                    if (!selectedCategories.contains(quoteType)) selectedCategories
-                                        .add(quoteType)
-                                }
-                            },
-                        )
                         Text(
-                            modifier = Modifier.padding(start = 2.dp),
+                            modifier = Modifier.padding(
+                                horizontal = 8.dp,
+                                vertical = 10.dp
+                            ),
                             text = category.quote_name
                         )
                     }
@@ -79,12 +114,14 @@ fun ChooseCategoryScreen(
             }
         }
 
-        Button(onClick = {
-            CoroutineScope(Dispatchers.IO).launch {
-                viewModel.updateQuotesByType(selectedCategories)
-            }
-            navController.navigate(AppScreens.HomeScreen.name)
-        }) {
+        Button(
+            modifier = Modifier.padding(10.dp),
+            onClick = {
+                CoroutineScope(Dispatchers.IO).launch {
+                    viewModel.updateQuotesByType(selectedCategories)
+                }
+                navController.navigate(AppScreens.HomeScreen.name)
+            }) {
             Text("Save")
         }
     }
